c68f9a50843b6432bac95dab3087a0d5a941b47c
Takayuki Itou <31656325+TakaI2@users.noreply.github.com>
2018-01-26 01:36:41 +0900

    キャラの頭の上にプレイヤー名を表示。 カメラがキャラを追従するようにした。

---
+++ Assets/Script/OperationName.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.MyCompany.MyGame
+{
+    public class OperationName : MonoBehaviour
+    {
+        private GameObject namePlate; // 名前を表示しているプレート
+        public Text nameText; //名前を表示するテキスト
+
+        // Use this for initialization
+        void Start()
+        {
+            namePlate = nameText.transform.parent.gameObject;
+        }
+
+
+        void LateUpdate()
+        {
+            namePlate.transform.rotation = Camera.main.transform.rotation;
+        }
+
+        // Update is called once per frame
+        [PunRPC]
+        void SetName(string name)
+        {
+            nameText.text = name;
+        }
+    }
+
+}
+
