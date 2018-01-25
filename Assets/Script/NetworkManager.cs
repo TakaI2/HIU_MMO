2956eefc823b77a3a8a8ae3dba0a8dfaa4ff7d7e
Takayuki Itou <31656325+TakaI2@users.noreply.github.com>
2018-01-25 13:07:04 +0900

    ネットワーク接続用スクリプトを追加。

---
+++ Assets/Script/NetworkManager.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+
+    public Text text;
+    public GameObject loginUI;
+    public Dropdown roomList;
+    public InputField roomName;
+    public InputField playerName;
+    public GameObject logoutUI;
+    public GameObject CameraPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //ログをすべて表示する。
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+
+        //ロビーに自動で入る
+        PhotonNetwork.autoJoinLobby = true;
+
+        //ゲームのバージョン設定
+        PhotonNetwork.ConnectUsingSettings("0.1");
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //サーバ接続状態を表示
+        text.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+
+    //マスターサーバに接続されたときに呼ばれる。
+    void OnConnectedToMaster()
+    {
+        Debug.Log("マスターサーバに接続");
+    }
+
+    //ロビーに入った時に呼ばれる。
+    void OnJoinedLobby()
+    {
+        Debug.Log("ロビーに入る");
+        loginUI.SetActive(true);
+    }
+
+    //ログインボタンを押したときに実行するメソッド
+    public void LoginGame()
+    {
+        //ルームオプションを設定
+        RoomOptions ro = new RoomOptions();
+        //ルームを見えるようにする
+        ro.IsVisible = true;
+        //部屋の入室最大人数
+        ro.MaxPlayers = 10;
+
+        if (roomName.text != "")
+        {
+            //部屋がない場合は作って入室
+            PhotonNetwork.JoinOrCreateRoom(roomName.text, ro, TypedLobby.Default);
+
+        }
+        else
+        {
+            //部屋が存在すれば
+            if (roomList.options.Count != 0)
+            {
+                Debug.Log(roomList.options[roomList.value].text);
+                PhotonNetwork.JoinRoom(roomList.options[roomList.value].text);
+                //部屋が存在しなければDefaultRoomという名前で部屋を作成
+            }
+            else
+            {
+                PhotonNetwork.JoinOrCreateRoom("DefaultRoom", ro, TypedLobby.Default);
+            }
+        }
+
+    }
+
+    //部屋が更新されたときの処理
+    void OnReceivedRoomListUpdate()
+    {
+        Debug.Log("部屋更新");
+
+        //部屋情報を取得する
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+        //ドロップダウンリストに追加する文字列用のリストを作成
+        List<string> list = new List<string>();
+
+        //部屋情報を部屋リストに表示
+        foreach (RoomInfo room in rooms)
+        {
+            //部屋が満員でなければ追加
+            if (room.PlayerCount < room.MaxPlayers)
+            {
+                list.Add(room.Name);
+            }
+        }
+
+        //ドロップダウンリストをリセット
+        roomList.ClearOptions();
+
+        //部屋が一つでもあればドロップダウンリストに追加
+        if (list.Count != 0)
+        {
+            roomList.AddOptions(list);
+        }
+
+    }
+
+    //部屋に入室したときに呼ばれるメソッド
+    void OnJoinedRoom()
+    {
+        loginUI.SetActive(false);
+        logoutUI.SetActive(true);
+        Debug.Log("入室");
+
+        //InputFieldに入力した名前を設定
+        if (playerName.text != "")
+        {
+            PhotonNetwork.player.NickName = playerName.text;
+        }
+        else
+        {
+            PhotonNetwork.player.NickName = "DefaultPlayer";
+        }
+
+        // ログインを監視する
+        StartCoroutine("SetPlayer", 0f);
+
+    }
+
+
+    // プレイヤーをゲームの世界に出現させる
+    IEnumerator SetPlayer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        // ネットワークごしにキャラをインスタンス化
+        GameObject player = PhotonNetwork.Instantiate("BoxBody", Vector3.up, Quaternion.identity, 0);
+
+        player.GetPhotonView().RPC("SetName", PhotonTargets.AllBuffered, PhotonNetwork.player.NickName);
+    }
+
+
+
+    //部屋の入室に失敗した
+    void OnPhotonJoinRoomFailed()
+    {
+        Debug.Log("入室に失敗");
+
+        //ルームオプションを設定
+        RoomOptions ro = new RoomOptions();
+        //ルームを見えるようにする
+        ro.IsVisible = true;
+        //部屋の最大入室人数
+        ro.MaxPlayers = 10;
+        //入室に失敗したらDefaultRoomを作成し入室
+        PhotonNetwork.JoinOrCreateRoom("DefaultRoom", ro, TypedLobby.Default);
+    }
+
+
+    //LogoutButtonを押したときの処理
+    public void LogoutGame()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    //部屋を退室したときの処理
+    void OnLeftRoom()
+    {
+        Debug.Log("退室");
+        logoutUI.SetActive(false);
+    }
+
+
+}
